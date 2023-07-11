9bf76f4f3e02bd0661351a70106530eeb0a9a02a
Stefi <stefi.says.hi@gmail.com>
2023-07-11 19:01:12 -0300

    Update mirall0x.py

--- main/main/mirall0x.py
+++ main/main/mirall0x.py
@@ -382,7 +382,7 @@ def website_validation_lego(website_lists):
 covalent_chains = ['eth-mainnet' , 'optimism-mainnet' , 'matic-mainnet' , 'btc-mainnet' ] ## issue #12
   
 api_key = st.secrets['covalent_api']
-git_PAT = st.secrets['git_PAT']
+git_PAT = st.secrets['github_PAT']
 #________________________ main __________________________________________________________________________
 
 
